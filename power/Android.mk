f277a660cb81515674c222216b80cd5315b99549
Neil Agarwal <regalstreak@gmail.com>
2017-07-09 15:06:33 +0200

    power: Add libxml2 dependency
    Change-Id: I0612025a23b17a2c8ceae683f3ffa37046740868
    Check: https://review.halogenos.org/#/c/3550/
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- power/Android.mk
+++ power/Android.mk
@@ -13,7 +13,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2
 LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 
 ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
