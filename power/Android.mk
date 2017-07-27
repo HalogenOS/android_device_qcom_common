8c2d73ad985980a93b44f249d1ccdc22a92427e5
Simao Gomes Viana <xdevs23@outlook.com>
2017-07-28 00:17:28 +0200

    Revert "qcom: power: add msm8998 target information"
    This reverts commit 6f43f64f01c70d6b7440f2e454ccdb4c4565dd5a.

--- power/Android.mk
+++ power/Android.mk
@@ -14,9 +14,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_PROPRIETARY_MODULE := true
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl
-LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c
-LOCAL_C_INCLUDES := external/libxml2/include \
-                    external/icu/icu4c/source/common
+LOCAL_SRC_FILES := power.c metadata-parser.c utils.c list.c hint-data.c
 
 ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
   LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_POWER_CUSTOM_BOARD_LIB)
@@ -71,10 +69,6 @@ endif
 ifeq ($(call is-board-platform-in-list, msm8996), true)
 LOCAL_SRC_FILES += power-8996.c
 LOCAL_CFLAGS += -DMPCTLV3
-endif
-
-ifeq ($(call is-board-platform-in-list,msm8998), true)
-LOCAL_SRC_FILES += power-8998.c
 endif
 
 endif  #  End of board specific list
