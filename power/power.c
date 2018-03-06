HalogenOS/android_device_qcom_common

commit 4be048e887818659cabb2e1bb0c5adcbf0d4c2c7

power: Make powerHAL compatible for passthrough HIDL design
Bug: 31177288
Change-Id: Ia65f6b50d091a02199565af1d8855ec7473e7323
Signed-off-by: Ruchi Kandoi <kandoiruchi@google.com>
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

power/power.c
@@ -497,6 +497,46 @@ int get_feature(struct power_module *module __unused, feature_t feature)
     return -1;
 }
 
+static int power_open(const hw_module_t* module, const char* name,
+                    hw_device_t** device)
+{
+    ALOGD("%s: enter; name=%s", __FUNCTION__, name);
+    int retval = 0; /* 0 is ok; -1 is error */
+
+    if (strcmp(name, POWER_HARDWARE_MODULE_ID) == 0) {
+        power_module_t *dev = (power_module_t *)calloc(1,
+                sizeof(power_module_t));
+
+        if (dev) {
+            /* Common hw_device_t fields */
+            dev->common.tag = HARDWARE_DEVICE_TAG;
+            dev->common.module_api_version = POWER_MODULE_API_VERSION_0_3;
+            dev->common.hal_api_version = HARDWARE_HAL_API_VERSION;
+
+            dev->init = power_init;
+            dev->powerHint = power_hint;
+            dev->setInteractive = set_interactive;
+            dev->setFeature = set_feature;
+            dev->getFeature = get_feature;
+            dev->get_number_of_platform_modes = NULL;
+            dev->get_platform_low_power_stats = NULL;
+            dev->get_voter_list = NULL;
+
+            *device = (hw_device_t*)dev;
+        } else
+            retval = -ENOMEM;
+    } else {
+        retval = -EINVAL;
+    }
+
+    ALOGD("%s: exit %d", __FUNCTION__, retval);
+    return retval;
+}
+
+static struct hw_module_methods_t power_module_methods = {
+    .open = power_open,
+};
+
 struct power_module HAL_MODULE_INFO_SYM = {
     .common = {
         .tag = HARDWARE_MODULE_TAG,

commit 8bf98ac0f066bc4004aff5b8e244b67672138d3e

power: Remove redeclaration of power_module_methods
Fixes: 4be048e88781 ("power: Make powerHAL compatible for passthrough HIDL design")
Change-Id: I65846a77c8b29352c6ac90b877e9d592615ff4a8
Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

power/power.c
@@ -60,10 +60,6 @@ static int display_hint_sent;
 
 static char tap_to_wake_node_dyn[NODE_MAX];
 
-static struct hw_module_methods_t power_module_methods = {
-    .open = NULL,
-};
-
 static pthread_mutex_t hint_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 static void power_init(__attribute__((unused))struct power_module *module)